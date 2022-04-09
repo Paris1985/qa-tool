29e66f199b029b5fce406108780d02eb050ded6c
Paris1985 <bobby.paris85@gmail.com>
2022-04-09 20:55:28 +0800

    Adds cucumber props

--- src/test/java/org/qa/tool/cucumber/CucumberTest.java
+++ src/test/java/org/qa/tool/cucumber/CucumberTest.java
@@ -3,11 +3,16 @@ package org.qa.tool.cucumber;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.BeforeClass;
 
 @CucumberOptions(
         features = "classpath:features",
         glue = {"org.qa.tool.cucumber.steps" },
-        plugin = {"pretty", "json:target/report.json"})
+        plugin = {"pretty", "json:target/report.json"},
+        publish = true)
 public class CucumberTest extends AbstractTestNGCucumberTests {
-
+    @BeforeClass
+    public static void init() {
+        System.setProperty("CUCUMBER_PUBLISH_ENABLED", "true");
+    }
 }
